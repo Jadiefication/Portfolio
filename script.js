287fca9dab2908794fbc1695c822cb7a787f334c
Jadiefication <143262080+Jadiefication@users.noreply.github.com>
2024-03-04 20:51:07 +0100

    Added new buttons, still working on functionality

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-//when the function is called it changes the text of an element with an id text to changed
 
-function changetxt(){
-    document.getElementById("text").innerHTML = "Changed";
+function cya() {
+    window.alert("You are planning to leave this page. Are you sure?");
 }
+
+function nuke() {
+    document.getElementById("text").textContent = ""
+    document.getElementById("text2").textContent = ""
+    document.getElementById("text3").textContent = ""
+}
+
+function recover() {
+    document.getElementById("text"). =
+    document.getElementById("text2").textContent = "Hello my name is Jadiefication and I'm a starting python developer from the Czech Republic.I have been programming for about 6 months, but in the time I managed to get some good projects.If you wish to see some of my projects check out my github, and a repository named learning."
+    document.getElementById("text3").textContent = "PLEASE DO NOTE: This site is still in progress, things may change(most definitely will) so the website won't look like now"
+}
+
